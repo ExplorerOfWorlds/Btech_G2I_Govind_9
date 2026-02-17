9bdecf333bafcff0b47458e81d1ce39ea5fe2a53
ExplorerOfWorlds <25116cn396@glbitm.ac.in>
2026-02-17 20:20:55 +0530

    grade.c
    added total and percentage

--- grade.c
+++ grade.c
@@ -1,20 +1,22 @@
 #include <stdio.h>
 void main(){
-    int a,b,c,d,e,total,grade;
+    int a,b,c,d,e,total,percent;
     printf("enter marks of 5 subject");
     scanf("%d%d%d%d%d",&a,&b,&c,&d,&e);
     total=a+b+c+d+e;
-    grade=total/5;
-    if (grade>=90){
-        printf("Grade A");
+    percent=total/5;
+    if (percent>=90){
+
+        printf("Grade A \n ");
     }
-    else if(grade<90 && grade>=80){
-        printf("Grade B");
+    else if(percent<90 && percent>=80){
+        printf("Grade B \n ");
     }
-    else if (grade<80 && grade>=60){
-        printf("Grade C");
+    else if (percent<80 && percent>=60){
+        printf("Grade C \n ");
     }
     else{
-        printf("Grade D")
+        printf("Grade D \n ");
     }
+    printf("Total: %d, Percentage: %d\n", total, percent);
 }
